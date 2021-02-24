af1d98325cb13a9ecd3705a1b8301fe53506c25b
timkos98 <timkos@live.at>
2021-02-24 05:58:39 +0000

    values updated to be unbounded

--- initialValuesForCBSS.py
+++ initialValuesForCBSS.py
@@ -2,19 +2,11 @@
 # Adjust the initial values below as needed
 
 # Breaker properties
-Breaker_OpenDelay_min = 0.03    # seconds
-Breaker_OpenDelay_max = 0.5     # seconds
-Breaker_OpenTimestep = 0.001    # seconds
+Breaker_OpenInitialValue = 0.04 # seconds **Must be in range**
 
-Breaker_CloseDelay_min = 0.04   # seconds
-Breaker_CloseDelay_max = 0.5     # seconds
-Breaker_CloseTimestep = 0.001   # seconds
+Breaker_CloseInitialValue = 0.04 # seconds **Must be in range**
 
 # Switch Properisties
-Switch_OpenDelay_min = 0.04     # seconds
-Switch_OpenDelay_max = 10       # seconds
-Switch_OpenTimestep = 0.001     # seconds
+Switch_OpenInitialValue = 0.04   # seconds **Must be in range**
 
-Switch_CloseDelay_min = 0.04    # seconds
-Switch_CloseDelay_max = 10       # seconds
-Switch_CloseTimestep =	0.1 	# seconds
+Switch_CloseInitialValue = 0.04  # seconds **Must be in range**
